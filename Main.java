d657432f89f503494c9bccdfd828f5a760c7e406
unknown <vuminhhieu22092001@gmail.com>
2023-09-13 15:08:33 +0700

    ngaythang

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
+import java.util.Date;
 public class Main {
-//    public static void main(String[] args) {
-//
-//    }
 public static void main(String[] args) {
     System.out.println("hello");
     System.out.println("xin chao");
+    Date date = new Date();
+    System.out.println("Time niw: "+ date);
 }
 }
